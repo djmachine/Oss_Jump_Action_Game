00e953d051c333abebb8ae06e9672954883084c6
CBNU-Park Jong Eun <107690940+JJJJJJONG@users.noreply.github.com>
2022-12-13 09:08:44 +0900

    Create Up_Down_Move.cs

---
+++ Up_Down_Move.cs
@@ -0,0 +1,55 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Up_Down_Move_Platform : MonoBehaviour
+{
+    public Transform startpos;
+    public Transform endpos;
+    public Transform despos;
+    public float speed;
+    void Start()
+    {
+        transform.position = startpos.position;
+        despos = endpos;
+
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+
+    //    if (collision.transform.CompareTag("Player"))
+    //    {
+    //        collision.transform.SetParent(transform);
+            
+    //    }
+    //}
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.transform.CompareTag("Player"))
+    //    {
+    //        collision.transform.SetParent(null);
+    //    }
+    //}
+
+void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, despos.position, Time.deltaTime * speed);
+        if (Vector2.Distance(transform.position, despos.position) <= 0.05f)
+        {
+            if (despos == endpos)
+            {
+                despos = startpos;
+            }
+
+            else
+            {
+                despos = endpos;
+            }
+        }
+    }
+
+}
+

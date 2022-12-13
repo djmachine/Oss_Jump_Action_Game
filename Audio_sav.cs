8e2e2ae1e05aef4eef5c962ac064a2e342c049f4
CBNU-Park Jong Eun <107690940+JJJJJJONG@users.noreply.github.com>
2022-12-13 09:05:12 +0900

    Create Audio_sav.cs

---
+++ Audio_sav.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Save : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+   
+}
